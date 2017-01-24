cd2f7ac3affe7a9f48fe3c17aa6885f568c38746
scannella <scannella@bishopireton.org>
2017-01-24 12:50:17 -0500

    First Edit More work left

---
+++ app/src/main/java/com/example/scannella/testhello/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.scannella.testhello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        final Button button = (Button) findViewById(R.id.btnPress);
+        final TextView word = (TextView) findViewById(R.id.txtHello);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                word.setText("Yo, Aidan");
+            }
+        });
+    }
+}
